9c12055679d791b69378ba39fa168b11d411755c
Alex Hansen <alex.hansen@chasacademy.se>
2024-04-18 10:18:11 +0200

    feat: error route now displays error, rm unused assets

--- src/routes/ErrorRoute.tsx
+++ src/routes/ErrorRoute.tsx
@@ -1,3 +1,26 @@
+import {
+  useRouteError,
+  ErrorResponse,
+  isRouteErrorResponse,
+  Link,
+} from "react-router-dom";
+
 export const ErrorRoute = () => {
-  return <main>Error!</main>;
+  const error = useRouteError();
+
+  return (
+    <main>
+      <h1>Error</h1>
+      {isRouteErrorResponse(error) && (
+        <p>
+          <i>
+            {(error as ErrorResponse).status +
+              " " +
+              (error as ErrorResponse).statusText}
+          </i>
+        </p>
+      )}
+      <Link to={"/"}>Return</Link>
+    </main>
+  );
 };
