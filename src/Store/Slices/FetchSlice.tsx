3fc636b9736eb3ae2c0158a0c37803aa521cf36d
Alex Hansen <alex.hansen@chasacademy.se>
2024-04-24 15:14:56 +0200

    feat: base for character list

--- src/Store/Slices/FetchSlice.tsx
+++ src/Store/Slices/FetchSlice.tsx
@@ -1,10 +1,9 @@
 //@ts-nocheck
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface DataState {
   entities: any[];
-  loading: 'idle' | 'loading' | 'succeeded' | 'failed';
+  loading: "idle" | "loading" | "succeeded" | "failed";
   error: string | null;
 }
 
@@ -12,44 +11,44 @@ interface FetchResponse {
   [key: string]: any;
 }
 
-export const fetchData = createAsyncThunk<FetchResponse, string, {rejectValue: string }>(
-    'data/fetchData',
-    async (url, thunkAPI) => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error('Network response was not ok');
-        console.log("KOLLA DET FUNKADE!!!! FÖR FAN!!!!", response)
-        const data = await response.json();
-        return data;
-      } catch (error: any) {
-        return thunkAPI.rejectWithValue(error.message);
-      }
-    }
-  );
-
+export const fetchData = createAsyncThunk<
+  FetchResponse,
+  string,
+  { rejectValue: string }
+>("data/fetchData", async (url, thunkAPI) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Network response was not ok");
+    // console.log("KOLLA DET FUNKADE!!!! FÖR FAN!!!!", response)
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
 
 const dataSlice = createSlice({
-  name: 'data',
+  name: "data",
   initialState: {
     entities: [],
-    loading: 'idle',
-    error: null
+    loading: "idle",
+    error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchData.pending, (state) => {
-        state.loading = 'loading';
+        state.loading = "loading";
       })
       .addCase(fetchData.fulfilled, (state, action) => {
-        state.loading = 'succeeded';
+        state.loading = "succeeded";
         state.entities = action.payload;
       })
       .addCase(fetchData.rejected, (state, action: PayloadAction<string>) => {
-        state.loading = 'failed';
+        state.loading = "failed";
         state.error = action.payload;
       });
-  }
+  },
 });
 
 export default dataSlice.reducer;
--- src/routes/CharactersRoute.tsx
+++ src/routes/CharactersRoute.tsx
@@ -1,3 +1,93 @@
+import style from "./CharactersRoute.module.css";
+
+export type Character = {
+  name: string;
+  age: number;
+  gender: string; //should be enum/string literal?
+  class: string; //should be enum/string literal?
+  level: number;
+  hitpoints: number;
+  strength: number;
+  dexterity: number;
+  intelligence: number;
+  wisdom: number;
+  constitution: number;
+  charisma: number;
+  backstory: string;
+  professionName: null; //should be enum/string literal?
+  speciesName: null; //should be enum/string literal?
+  //image? (url)
+};
+
 export const CharactersRoute = () => {
-  return <main>characters</main>;
+  const characters: Character[] = [
+    {
+      name: "Foo",
+      age: 26,
+      gender: "male",
+      class: "Wizard",
+      level: 1,
+      hitpoints: 10,
+      strength: 10,
+      dexterity: 10,
+      intelligence: 10,
+      wisdom: 10,
+      constitution: 10,
+      charisma: 10,
+      backstory: "Bondafan",
+      professionName: null,
+      speciesName: null,
+    },
+    {
+      name: "Bar",
+      age: 30,
+      gender: "female",
+      class: "Rogue",
+      level: 5,
+      hitpoints: 10,
+      strength: 10,
+      dexterity: 10,
+      intelligence: 10,
+      wisdom: 10,
+      constitution: 10,
+      charisma: 10,
+      backstory: "Arbetslös",
+      professionName: null,
+      speciesName: null,
+    },
+  ]; //TODO: get from server
+
+  const handleCharacterClicked = (character: Character) => {
+    //TODO: set selected character and move on to next route in flow
+    console.log(character.name, "selected!");
+  };
+
+  return (
+    <main>
+      <h1>Your Characters</h1>
+      <ul className={style.charactersList}>
+        {characters.map((character, index) => {
+          return (
+            <li
+              className={style.character}
+              key={character.name + index}
+              onClick={() => handleCharacterClicked(character)}
+            >
+              <img
+                className={style.characterIcon}
+                src="/images/defaultCharacterIcon.png" /*TODO: load actual image and have default as error*/
+                alt={`${character.name}`}
+              />
+              <h2>{character.class}</h2>
+              <h2>{character.name}</h2>
+              <h2>lvl {character.level}</h2>
+            </li>
+          );
+        })}
+      </ul>
+      <div>
+        <button type="button">Create</button>
+      </div>
+    </main>
+  );
 };
