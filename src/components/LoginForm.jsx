15e4431707e93c6b864c45321de4906695673199
robinruden <145325673+robinruden@users.noreply.github.com>
2024-04-25 15:25:17 +0200

    nu kan man logga in lite snyggare

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,42 +1,56 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
+import './LoginForm.css'; // Ensure this path is correct
 
-function LoginForm(){
-    const [username, setUsername] = useState ('')
-    const [password, setPassword] = useState('')
+function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleLogin = async (event) => {
-        event.preventDefault()
+        event.preventDefault();
 
-    try {
-        const response = await axios.post('URL', {
-            username: username,
-            password: password
-        })
+        try {
+            const response = await axios.post('URL', {
+                username: username,
+                password: password
+            });
 
-        if (response.data.token){
-            localStorage.setItem('token', response.data.token)
-            console.log("Login lyckades!!!")
-        } else {
-            console.log("Login mysslyckades!!!", response.data.message)
+            if (response.data.token) {
+                localStorage.setItem('token', response.data.token);
+                console.log("Login lyckades!!!");
+            } else {
+                console.log("Login mysslyckades!!!", response.data.message);
+            }
+        } catch (error) {
+            console.error("Login error!!!", error.response.data);
         }
-        
-    } catch (error){
-        console.error("Login error!!!", error.response.data)
-    }
-}
+    };
+
     return (
-        <form onSubmit={handleLogin}>
-            <div>
-                <label>Username:</label>
-                <input type="text" 
+        <div className="form-container">
+            <form className="form" onSubmit={handleLogin}>
+                <div>
+                    <label className="label">Username:</label>
+                    <input
+                        className="input"
+                        type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <button type="submit">Logga in</button>
-        </form>
-    )
+                    />
+                </div>
+                <div>
+                    <label className="label">Password:</label>
+                    <input
+                        className="input"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button className="button" type="submit">Logga in</button>
+            </form>
+        </div>
+    );
 }
 
-export default LoginForm
+export default LoginForm;
