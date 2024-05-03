af514841406b0988d1ae9062527caf67b38de93f
julia.paulin <julia.paulin@chasacademy.se>
2024-05-03 10:05:36 +0200

    json and styling

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,33 +1,23 @@
-import { getStory } from "../api/storiesApi";
-import { createStoryId } from "../api/saveId";
-import { useState, useEffect } from "react";
-import { Story, StoryId } from "../types/types";
+import { promptStoryText } from "../api/storiesApi";
+import { Story, StoryText } from "../types/types";
 import { StoryItem } from "./StoryItem";
+import premadeStories from "../api/stories.json";
 
 export const Stories = () => {
-  const [stories, setStories] = useState<Story[]>([]);
-  //const [story, setStory] = useState<string>("");
 
-  useEffect(() => {
-    const fetchStory = async () => {
-      const allStories = await getStory();
-      setStories(allStories);
+  const onSelected = (story: string) => {
+    const storyText: StoryText = {
+      text: story,
     };
-    fetchStory();
-  }, []);
-
-  const onSelected = (id: string) => {
-    const storyId: StoryId = {
-      id: id,
-    };
-    createStoryId(storyId);
+    promptStoryText(storyText);
+    console.log(storyText);
   };
 
   return (
     <>
       <div>
         <ul>
-          {stories.map((story: Story) => {
+          {premadeStories.map((story: Story) => {
             return (
               <StoryItem key={story.id} story={story} onSelected={onSelected} />
             );
