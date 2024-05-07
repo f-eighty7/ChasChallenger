afa8d5dfda2403405b8f60aee4bdc55f3672ff78
carolinnepmelo@gmail.com <carolinnepmelo@gmail.com>
2024-05-07 12:21:29 +0200

    add style to title

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -1,13 +1,39 @@
 import { Link } from "react-router-dom";
+import ButtonGradientBlue from "../components/ButtonGradientBlue";
 
 export const HomeRoute = () => {
   return (
     <main>
-      <h1 style={{ padding: "1rem", marginTop: "5rem", marginBottom: "3rem" }}>
-        Master Chass
-      </h1>
+      <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: "center",
+        
+      }}
+      >
+      <h2 style={{ 
+          fontFamily: '"Cinzel Decorative", serif',
+          fontSize: '3.4rem',
+          padding: '0',
+          margin: '0',
+          textShadow: '4px 4px 6px rgba(0, 0, 0, 0.8)',
+
+
+        }}>
+        MasteR 
+      </h2>
+        <h1 style={{ 
+          fontFamily: '"Cinzel Decorative", serif',
+          fontSize: '5rem',
+          lineHeight: '80%',
+          textShadow: '4px 4px 6px rgba(0, 0, 0, 0.8)',
+        }}>
+          ChasS
+        </h1>
+      </div>
       <Link to={"/welcome"}>
-        <button>Start</button>
+        <ButtonGradientBlue buttonText={'Start'}/>
       </Link>
     </main>
   );
