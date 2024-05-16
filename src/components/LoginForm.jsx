24190642901a303262f4ff675caa90ef6dc481a3
robinruden <145325673+robinruden@users.noreply.github.com>
2024-05-16 15:09:55 +0200

    nu funkar skiten

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./LoginForm.css";
 import { Link, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+/* import { Link } from "react-router-dom"; */
 import ButtonOne from "./ButtonOne";
 
 axios.defaults.withCredentials = true
