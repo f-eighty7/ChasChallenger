2648d9875125fcdbe7165fe9e441a3a085c2c6f7
robinruden <145325673+robinruden@users.noreply.github.com>
2024-05-27 15:44:11 +0200

    http

--- src/routes/CharactersRoute.tsx
+++ src/routes/CharactersRoute.tsx
@@ -30,7 +30,7 @@ const getCharacters = async (): Promise<Character[] | undefined> => {
   try {
     const response = await axios.get(
       /* "http://localhost:5106/api/Character/GetCharacters" */
-      `https://localhost:7110/api/Character/GetCharacters` 
+      `http://localhost:7110/api/Character/GetCharacters` 
     );
 
     return await response.data;
