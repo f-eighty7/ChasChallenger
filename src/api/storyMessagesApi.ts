dadc1e015cbd5174c9933ac021876c753bd765c2
robinruden <145325673+robinruden@users.noreply.github.com>
2024-05-31 12:52:45 +0200

    bra api

--- src/api/storyMessagesApi.ts
+++ src/api/storyMessagesApi.ts
@@ -3,13 +3,14 @@ import axios from "axios";
 // const BASE_URL =
 //   "https://localhost:7110/api/Chat/Chathistory?activeStoryId=5&amountPerPage=20&pageNumber=1";
 //   `https://chasfantasy.azurewebsites.net/api/Chat/Chathistory?activeStoryId=10&amountPerPage=10&pageNumber=1`
-`/api/Chat/Chathistory?activeStoryId=10&amountPerPage=10&pageNumber=1`;
+/* `/api/Chat/Chathistory?activeStoryId=10&amountPerPage=10&pageNumber=1`; */
 
 //READ
 export const getStoryMessages = async (id: number) => {
   try {
     const response = await axios.get(
-      `https://chasfantasy.azurewebsites.net/api/Chat/Chathistory?activeStoryId=${id}&amountPerPage=10&pageNumber=1`
+      `/api/Chat/Chathistory?activeStoryId=${id}&amountPerPage=10&pageNumber=1`
+      /* `https://chasfantasy.azurewebsites.net//api/Chat/Chathistory?activeStoryId=${id}&amountPerPage=10&pageNumber=1` */
     );
     return response.data;
   } catch (error) {
